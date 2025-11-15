7d94ae2245a11db2ffd16985db78e29fbde14d67
Abdelrhman Mahmoud <118855701+Abdelrhmanx74@users.noreply.github.com>
2025-11-15 15:36:50 +0200

    Remove obsolete WAD handling code and associated resources
    - Deleted `mounted.hpp`, `toc.cpp`, and `toc.hpp` files which contained definitions and implementations for WAD mounting and table of contents handling.
    - Removed manifest files `longpath.manifest` and `utf8.manifest` that were no longer needed.
    - Eliminated diagnostic and mod tools source files (`main_diag.cpp`, `main_mod_tools.cpp`, `main_wad_extract.cpp`, `main_wad_make.cpp`) as they are outdated and not in use.

--- src/cslol-tools/lib/lol/log.hpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <fmtlog.h>
-
-#include <lol/common.hpp>
-
-namespace lol {
-    void init_logging_thread();
-}
--- src/cslol-tools/lib/lol/utility/zip.hpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <lol/common.hpp>
-#include <lol/fs.hpp>
-
-namespace lol::utility {
-    extern auto zip(fs::path const& src, fs::path const& dst) -> void;
-    extern auto unzip(fs::path const& src, fs::path const& dst) -> void;
-}
